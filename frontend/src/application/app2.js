4b78888e73621aa3207a74ba9e86e5ee7d963a39
darkwolf <darkwolf1990janary@gmail.com>
2023-09-28 22:42:30 +0300

    frontend

---
+++ frontend/src/application/app2.js
@@ -0,0 +1,10 @@
+// import scss entry file here for app2.js
+
+// import other JS components as we like
+import "../components/sidebar";
+
+
+window.document.addEventListener("DOMContentLoaded", function () {
+  window.console.log("dom ready 2");
+});
+
